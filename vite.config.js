7bb6ae9e4d7df8f69a739e8b0ae3a85e2d45dc0d
Tandc24v7k025 <tandc24v7k025@vlvh.ctu.edu.vn>
2026-05-07 20:57:41 +0700

    Khoi tao du an

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { Server } from 'node:http'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    port: 3001,
+  }
+})
