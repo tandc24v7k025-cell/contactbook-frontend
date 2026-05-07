122090f43220685805a0977cd924c76afa02ccb5
Tandc24v7k025 <tandc24v7k025@vlvh.ctu.edu.vn>
2026-05-08 00:34:06 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    port: 3001,
-  }
-})
+    server: {
+      port: 3001,
+      proxy: {
+        "/api": {
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+        },
+      }
+  },
+});
